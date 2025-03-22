557b3f262be267adfae288a58eeaa8b04f37b83a
User <user@Users-MacBook-Pro.local>
2025-03-22 20:55:18 +0200

    redefind url logic

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { HttpClientService } from 'src/http-client/http-client.service';
 import { EVENT_SOURCES } from 'src/config/events.config';
 import { scrapeEvents } from 'src/common/utils/scraper';
 
@@ -15,11 +14,19 @@ export class EventService {
     search: string,
   ): Promise<Event[]> {
     const results: Event[] = [];
+    let url: string | undefined;
 
     for (const source of EVENT_SOURCES) {
-      const url = `${source.baseUrl}/d/${location}/${price + '--'}${date}/${search}/?page=${page}`;
-      const events: Event[] = await scrapeEvents(url, source.selectors);
-      results.push(...events);
+      if (source.name === 'eventbrite') {
+        url = `${source.baseUrl}/d/${location}/${price + '--'}${date}/${search}/?page=${page}`;
+      }
+
+      if (url) {
+        const events: Event[] = await scrapeEvents(url, source.selectors);
+        results.push(...events);
+      } else {
+        console.warn(`URL is undefined for source: ${source.name}`);
+      }
     }
 
     return results;
